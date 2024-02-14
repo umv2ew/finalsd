b102f5fa45eee8144ab3e22b4986c72e69417656
Lilla Király <lilla.kiraly@tnt-systems.eu>
2024-02-14 17:19:41 +0100

    dockerfiles

--- ApiGateway/sd.ApiGateway.Web/Program.cs
+++ ApiGateway/sd.ApiGateway.Web/Program.cs
@@ -22,4 +22,38 @@ app.UseEndpoints(endpoints =>
     endpoints.MapReverseProxy();
 });
 
+
+// https://github.com/microsoft/reverse-proxy/blob/2593b4c009f2c3a131cd4bf327115976aad2d7a6/samples/ReverseProxy.Direct.Sample/Startup.cs#L98-L99
+
+//// Configure our own HttpMessageInvoker for outbound calls for proxy operations
+//var httpClient = new HttpMessageInvoker(new SocketsHttpHandler()
+//{
+//    UseProxy = false,
+//    AllowAutoRedirect = false,
+//    AutomaticDecompression = DecompressionMethods.None,
+//    UseCookies = false
+//});
+
+//// Setup our own request transform class
+//var transformer = new CustomTransformer(); // or HttpTransformer.Default;
+//var requestOptions = new RequestProxyOptions { Timeout = TimeSpan.FromSeconds(100) };
+
+//app.UseEndpoints(endpoints =>
+//{
+//    // When using IHttpProxy for direct proxying you are responsible for routing, destination discovery, load balancing, affinity, etc..
+//    // For an alternate example that includes those features see BasicYarpSample.
+//    endpoints.Map("/{**catch-all}", async httpContext =>
+//    {
+//        await httpProxy.ProxyAsync(httpContext, "https://example.com", httpClient, requestOptions, transformer);
+//        var errorFeature = httpContext.Features.Get<IProxyErrorFeature>();
+
+//        // Check if the proxy operation was successful
+//        if (errorFeature != null)
+//        {
+//            var error = errorFeature.Error;
+//            var exception = errorFeature.Exception;
+//        }
+//    });
+//});
+
 app.Run();
